783ede7f0df71df731a505fecd74fe3a28ea2cc7
Raven <RavensKrag@gmail.com>
2022-04-05 23:10:47 -0400

    Create new div to use as a toolbar if the editable area is clicked.

--- index.js
+++ index.js
@@ -10,5 +10,24 @@ console.log("init page");
 
 
 bindButtonListener("editorjs", function(id, e){
-  console.log("area clicked:", id);
+  console.log("area clicked:", id, e);
+  
+  editor_id = "editor-toolbar";
+  
+  toolbar_node = document.getElementById(editor_id);
+  // => return 'null' if element with ID not found
+  console.log(toolbar_node);
+  
+  if(toolbar_node == null){
+    // create a new toolbar
+    node = document.getElementsByTagName("body")[0];
+      child = document.createElement("div");
+      child.appendChild(document.createTextNode("hello world!"));
+      child.id = editor_id;
+    node.appendChild(child);
+  }else{
+    // do things with the toolbar
+  }
+    
+  
 });
