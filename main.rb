bb0ee43c90b8ac153c9aa1a54505199f64265814
Raven <RavensKrag@gmail.com>
2022-04-05 21:33:13 -0400

    Local ruby server injects live.js to the page (for development) but deployed page does not (for performance)

--- main.rb
+++ main.rb
@@ -1,21 +1,44 @@
 #!/usr/bin/env ruby
-require 'sinatra'
+require 'bundler'
+Bundler.require
 
 puts "Sinatra starting soon"
 puts "webpage will be available at: http://localhost:4567/"
 
+
+
+
+
+
+# 
+# send webpage
+# 
 get '/' do
-	send_file 'index.html'
+  f = File.read('index.html')
+  script_tag = '<script type="text/javascript" src="live.js"></script>'
+  
+  doc = Nokogiri::HTML(f)
+  
+  doc.css('head')
+  .children.last
+  .add_next_sibling "  #{script_tag}\n" # add proper indentation
+  
+  doc.to_html
 end
 
 get '/profile_pic.png' do
-	send_file 'profile_pic.png'
+  send_file 'profile_pic.png'
 end
 
 get '/style.css' do
-	send_file 'style.css'
+  send_file 'style.css'
 end
 
 get '/semantic/*' do |name|
-	send_file "semantic/#{name}"
+  send_file "semantic/#{name}"
 end
+
+
+# 
+# JSON REST API endpoint
+# 
