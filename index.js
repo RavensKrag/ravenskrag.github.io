6155e445803a3e4871cb0f9e6f2f6ae579f46d6b
Raven <RavensKrag@gmail.com>
2022-04-05 23:59:14 -0400

    Always position the toolbar in a fairly reasonable place.

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bindButtonListener("editorjs", function(clickedArea, e){
   
   toolbar_node = document.getElementById(editor_id);
   // => return 'null' if element with ID not found
-  console.log(toolbar_node);
+  // console.log(toolbar_node);
   
   if(toolbar_node == null){
     // create a new toolbar
@@ -38,17 +38,52 @@ bindButtonListener("editorjs", function(clickedArea, e){
     node.appendChild(child);
   }else{
     // do things with the toolbar
+    
+    
+    
+    toolbar_width = 
+          window
+          .getComputedStyle(toolbar_node)
+          .getPropertyValue('width')
+          .match(/\d+/);
+    
+    body = document.getElementsByTagName("body")[0];
+    
+    rem = window
+          .getComputedStyle(body)
+          .getPropertyValue('font-size')
+          .match(/\d+/);
+    
+    // move the toolbar
     toolbar_node.setAttribute(
       "style", 
-      `left: ${e.clientX}px; top: ${e.clientY}px`
+      `left: ${e.target.offsetLeft + e.target.offsetWidth/2 - toolbar_width/2}px; top: calc(${e.target.offsetTop}px - ${1.5*rem}px)`
     );
+    
+    
   }
   
   
+  // process the text
+  // (click event returns text node)
   
-  x = window.getSelection()
-  console.log(x);
+  selection = window.getSelection()
+  // console.log(selection);
   
+  // for now, can't deal with selections that cross boundaries of different tags (like from h2 into p)
+  if(selection.anchorNode == selection.focusNode){
+    // https://stackoverflow.com/questions/6328718/how-to-wrap-surround-highlighted-text-with-an-element
+    
+    let sel = selection.getRangeAt(0);
+      let selectedContent = sel.extractContents();
+      var span = document.createElement("span");
+      span.style.backgroundColor = "lightpink";
+      span.appendChild(selectedContent);
+    sel.insertNode(span);
+    
+    
+    // selection.anchorOffset
+  }
   
     
   
